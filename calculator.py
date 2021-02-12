91d8b1ee22f794d046526db35cd55cc233d979c8
UlisPaxton <bodyu_rv@mail.ru>
2021-02-12 20:24:39 +0300

    Завершение работы над консольным интерфейсом, добавление коментариев и описания.

--- calculator.py
+++ calculator.py
@@ -13,20 +13,37 @@ Craft.add('Канистра Экспедиционная топливная (60/
           [*repeat('Бензиновая зажигалка Zibbo', 10), *repeat('Зажигалка Crickent', 10)], 1, 59)
 Craft.add('Граната "хаттабка" на базе ВОГ-25', 'VOG-25 Khattabka grenade',
           [*repeat('Запал УЗРГМ для гранат', 5), *repeat('40 мм ВОГ-25', 5)], 8, 64)
-
-# -------------------- корректировка цен, при желании можно скорректтировать цены
+"""
+ при желании можно скорректтировать цены и провести рассчёты исходя из указанных цен
+ 
 Craft.browser.cache.update({'Зажигалка Crickent': 12_000})
 Craft.browser.cache.update({'Бензиновая зажигалка Zibbo': 18_000})
 Craft.browser.cache.update({'Канистра Экспедиционная топливная (60/60)': 320_000})
 Craft.browser.cache.update({'Запал УЗРГМ для гранат': 11_000})
 Craft.browser.cache.update({'40 мм ВОГ-25': 16_000})
 Craft.browser.cache.update({'Граната "хаттабка" на базе ВОГ-25': 22_000})
+"""
+""" Пример вызова крафта
+# Craft.craft('Канистра Экспедиционная топливная (60/60)')
+# Craft.craft('Граната "хаттабка" на базе ВОГ-25')
+# Craft.craft('Водный фильтр')
 
-# -------------------- вызов крафта
-Craft.craft('Канистра Экспедиционная топливная (60/60)')
-Craft.craft('Граната "хаттабка" на базе ВОГ-25')
-Craft.craft('Водный фильтр')
+"""
 
+while True:
+    for count, item in enumerate(Craft.craft_table):
+        print(count, item['name'])
+    try:
+        item_number_to_craft = int(input('Выберите номер предмета для рассчёта:> '))
+        item_to_craft = Craft.craft_table[item_number_to_craft]
+        Craft.craft(item_to_craft['name'])
+    except IndexError:
+        print('Такого номера в списке нет.')
+    except ValueError:
+        print('Числовой номер, пожалуйста.')
+
+    if input("Ещё?(Y/N)") == 'N':
+        exit()
 
 
 
